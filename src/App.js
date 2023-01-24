990f478071a2371b4d737e98a24bcb0e44ee0e08
Alex <102092661+AziAlex@users.noreply.github.com>
2023-01-24 16:44:09 +0200

    furst
    необычная to do app для сохранение заметок

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { randomColor } from 'randomcolor'
+import Draggable from 'react-draggable'
 import './App.css';
 
 function App() {
+  const [item, setItem] = useState('')
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const newItem = () => {
+    if (item.trim() !== "") {
+      const newItem = {
+        id: uuidv4(),
+        item,
+        color: randomColor({
+          luminosity: 'light'
+        }),
+        defaultPos: {
+          x: 500,
+          y: -500
+        }
+      }
+      setItems((items) => [...items, newItem])
+      setItem('')
+    } else {
+      alert("Напиши что небуть...")
+      setItem('')
+    }
+  }
+
+
+
+  const deleteNow = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+
+  const updatePus = (data, index) => {
+    let newAray = [...items]
+    newAray[index].defaultPos = { x: data.x, y: data.y }
+    setItems(newAray)
+  }
+
+  const keyPress = (e) => {
+    const code = e.keyCode || e.which
+    if (code === 13) {
+      newItem()
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className='wraper'>
+        <input
+          value={item}
+          type="text"
+          placeholder='Напиши что небуть...'
+          onChange={(e) => setItem(e.target.value)}
+          onKeyPress={(e) => keyPress(e)}
+        />
+        <button
+          className='enter'
+          onClick={newItem}
         >
           Learn React
         </a>
